bf54638d5886dfb4ddbeaf43dd45d56861e0b127
fabioofertabv <ofertasbv@gmail.com>
2021-04-22 16:53:04 -0300

    criando autenticação oauth 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bofluttermobile/src/core/controller/categoria_controller.dart';
 import 'package:bofluttermobile/src/core/controller/cliente_controller.dart';
 import 'package:bofluttermobile/src/core/controller/loja_controller.dart';
@@ -60,7 +62,132 @@ class MyApp extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate
       ],
       supportedLocales: [const Locale('pt', 'BR')],
-      home: HomePage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) {
+            return HomePage();
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            child: buildContainer(context),
+          ),
+          SizedBox(height: 10),
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+
+  buildContainer(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              height: 80,
+              width: 80,
+              child: Container(
+                height: 80,
+                width: 80,
+                color: Colors.transparent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "BOOK",
+                            style: TextStyle(
+                              fontSize: 45,
+                              color: Theme.of(context).accentColor,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.shopping_basket_outlined,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        "OFERTAS",
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.transparent,
+                  child: ListTile(
+                    title: Text("versão 1.0"),
+                    subtitle: Text("todos os direitos reservados"),
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.transparent,
+                  child: ListTile(
+                    title: Text("contato"),
+                    subtitle: Text("bookofertasbr@gmail.com"),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/src/page/sobre/sobre_page.dart
+++ lib/src/page/sobre/sobre_page.dart
@@ -14,86 +14,90 @@ class _SobrePageState extends State<SobrePage> {
         titleSpacing: 0,
         title: Text("Sobre"),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              color: Theme.of(context).primaryColor,
+      body: buildContainer(context),
+    );
+  }
+
+  buildContainer(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            color: Theme.of(context).primaryColor,
+            child: Container(
+              height: 80,
+              width: 80,
               child: Container(
                 height: 80,
                 width: 80,
-                child: Container(
-                  height: 80,
-                  width: 80,
-                  color: Colors.transparent,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              "BOOK",
-                              style: TextStyle(
-                                fontSize: 45,
-                                color: Theme.of(context).accentColor,
-                                fontWeight: FontWeight.bold,
-                              ),
+                color: Colors.transparent,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "BOOK",
+                            style: TextStyle(
+                              fontSize: 45,
+                              color: Theme.of(context).accentColor,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Icon(
-                              Icons.shopping_basket_outlined,
-                              size: 50,
-                              color: Colors.white,
-                            ),
-                          ],
+                          ),
+                          Icon(
+                            Icons.shopping_basket_outlined,
+                            size: 50,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        "OFERTAS",
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Container(
-                        child: Text(
-                          "OFERTAS",
-                          style: TextStyle(
-                            fontSize: 40,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ),
             ),
-            Expanded(
-              child: Column(
-                children: [
-                  Container(
-                    height: 60,
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: ListTile(
-                      title: Text("versão 1.0"),
-                      subtitle: Text("todos os direitos reservados"),
-                    ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.transparent,
+                  child: ListTile(
+                    title: Text("versão 1.0"),
+                    subtitle: Text("todos os direitos reservados"),
                   ),
-                  Container(
-                    height: 60,
-                    width: double.infinity,
-                    color: Colors.transparent,
-                    child: ListTile(
-                      title: Text("contato"),
-                      subtitle: Text("bookofertasbr@gmail.com"),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                ),
+                Container(
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.transparent,
+                  child: ListTile(
+                    title: Text("contato"),
+                    subtitle: Text("bookofertasbr@gmail.com"),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
